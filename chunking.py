d3db9f7a0082f37ee71a9c14f1e3287ce58891fd
pneumatick <jhwwefan@gmail.com>
2026-07-15 12:08:57 -0400

    Prepare for chromadb implementation

--- chunking.py
+++ chunking.py
@@ -1,33 +1,44 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from enum import Enum, auto, unique
 from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
 
 @unique
 class Splitter(Enum):
     RECURSIVE = auto()
 
-def get_splitter(type):
-    if type == Splitter.RECURSIVE:
-        return RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-    else:
-        return None
+class VectorInterface():
+    def _get_splitter(self, type):
+        if type == Splitter.RECURSIVE:
+            return RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+        else:
+            return None
 
-def chunk_doc(document, split_type):
-    splitter = get_splitter(split_type)
+    def _chunk_doc(self, document, split_type):
+        splitter = self._get_splitter(split_type)
 
-    if not splitter:
-        return "Unknown splitter selected"
-    
-    chunks = splitter.split_text(document)
+        # NOTE: Handle error properly
+        if not splitter:
+            return "Unknown splitter selected"
+        
+        chunks = splitter.split_text(document)
 
-    return chunks
+        return chunks
 
-def chunk_docs(split_type):
-    path = Path("user-docs")
-    docs_string = []
-    for file_path in path.iterdir():
-        if file_path.is_file():
-            with open(file_path, "r", encoding="utf-8") as file:
-                docs_string.append(chunk_doc(file.read(), split_type))
+    def _chunk_docs(self, dirname, split_type):
+        path = Path(dirname)
+        docs_chunks = []
+        chunk_ids = []
+        for file_path in path.iterdir():
+            if file_path.is_file():
+                with open(file_path, "r", encoding="utf-8") as file:
+                    chunks = self._chunk_doc(file.read(), split_type)
+                    docs_chunks.append(chunks)
+                    for i, _ in enumerate(chunks):
+                        chunk_ids.append(str(file_path.name) + str(i))
 
-    return docs_string
+        return (docs_chunks, chunk_ids)
+
+    def add_docs(self, dirname):
+        (chunks, ids) = self._chunk_docs(dirname, Splitter.RECURSIVE)
+        return (chunks, ids)
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
-from chunking import Splitter, chunk_docs
+from chunking import Splitter, VectorInterface
 
 server = Flask(__name__)
 
 @server.route("/")
 def testing():
-    return chunk_docs(Splitter.RECURSIVE)
+    vec = VectorInterface()
+    (chunks, ids) = vec.add_docs("user-docs")
+    return ids + chunks
 
 if __name__ == "__main__":
     server.run(host='0.0.0.0', port="5000", debug=True)
